1002f629351292969d1e2108cd49e5023fccf906
Dario Cvitanović <41194638+TRDario@users.noreply.github.com>
2022-12-23 12:42:34 +0100

    Update optional_reference.hpp
    - Added nodiscard to all applicable functions
    - Added a const conversion operator
    - Added raw pointer interop (constructor, pointer getter)

--- optional_reference.hpp
+++ optional_reference.hpp
@@ -1,16 +1,15 @@
-/// @file
-/// @author Dario Cvitanović
+
+/// @brief Optional reference class and related functions.
 /// 
 /// @section License
-///
 /// Copyright (c) 2022 Dario Cvitanović
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 /// documentation files (the "Software"), to deal in the Software without restriction, including without limitation
-/// the rights to use, copy, modify, merge, publish, distribute, sublicense, and /or sell copies of the Software,
+/// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
 /// and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 ///
-/// The above copyright noticeand this permission notice shall be included in all copies or
+/// The above copyright notice and this permission notice shall be included in all copies or
 /// substantial portions of the Software.
 ///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
@@ -18,32 +17,19 @@
 /// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 /// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 /// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-/// 
-/// @section Description
-/// 
-/// This file contains an implementation of and several helper functions related to class optional_reference,
-/// a simple wrapper around a raw pointer with an interface similar to C++17's std::optional.
-/// The purpose is to be an idiomatic and safe alternative to classical non-owning raw pointers without
-/// needing to resort to clunky alternatives such as std::optional<std::reference_wrapper>.
-
-#if __cplusplus < 201402L
-static_assert(false, "optional_reference requires C++14 or higher.");
-#endif
 
 #ifndef DL_OPTIONAL_REFERENCE_HPP
 #define DL_OPTIONAL_REFERENCE_HPP
 
-#include <stdexcept>
-#include <memory>
-#ifndef NDEBUG
 #include <cassert>
-#endif
+#include <memory>
+#include <stdexcept>
 
 
 namespace dl {
 
   /// Empty class type used to indicate an optional_reference with an uninitialized state.
-  struct nullref_t {};
+  class nullref_t {};
 
   /// Constant of type nullref_t used to indicate an optional_reference with an uninitialized state.
   inline constexpr nullref_t nullref;
@@ -53,13 +39,15 @@ namespace dl {
   class bad_optional_reference_access : public std::exception {
 
   public:
+    /// Constructor.
     bad_optional_reference_access() noexcept {}
 
+    /// Brief explanation of the error.
     const char* what() const noexcept {
       return "bad optional reference access";
     }
 
-  };
+  }; // class bad_optional_reference_access
 
 
   /// Raw pointer wrapper with std::optional-like semantics and safety against null dereferencing.
@@ -68,26 +56,35 @@ namespace dl {
 
   public:
     /// Constructs an object that does not contain a reference.
-    constexpr optional_reference() noexcept
+    [[nodiscard]] constexpr optional_reference() noexcept
       : m_ptr(nullptr) {}
 
     /// Constructs an object that does not contain a reference.
-    constexpr optional_reference(nullref_t) noexcept
+    [[nodiscard]] constexpr optional_reference(nullref_t) noexcept
       : m_ptr(nullptr) {}
 
-    /// @brief Constructs an object containing a reference.
-    /// @param reference - the reference to be stored in the object.
-    constexpr optional_reference(T& reference) noexcept
+    /// Constructs an object containing a reference.
+    [[nodiscard]] constexpr optional_reference(T& reference) noexcept
       : m_ptr(std::addressof(reference)) {}
 
+    /// Constructs an object from a raw pointer.
+    [[nodiscard]] constexpr optional_reference(T* reference) noexcept
+      : m_ptr(reference) {}
 
-    /// Returns true if *this contains a reference, false if *this does not contain a reference.
-    constexpr explicit operator bool() const noexcept {
+
+    /// Const conversion operator.
+    [[nodiscard]] constexpr operator optional_reference<const T>() const noexcept {
+      return optional_reference<const T>(this);
+    }
+
+
+    /// Returns true if *this contains a reference, false otherwise.
+    [[nodiscard]] constexpr operator bool() const noexcept {
       return m_ptr;
     }
 
-    /// Returns true if *this contains a reference, false if *this does not contain a reference.
-    constexpr bool has_ref() const noexcept {
+    /// Returns true if *this contains a reference, false otherwise.
+    [[nodiscard]] constexpr bool has_ref() const noexcept {
       return m_ptr;
     }
 
@@ -95,27 +92,27 @@ namespace dl {
     /// @brief Returns the contained reference.
     /// @details Unlike optional_reference::ref, this operator is unchecked and will
     /// unapologetically dereference a null pointer without throwing an exception.
-    constexpr T& operator*() const noexcept {
-      #ifndef NDEBUG
+    [[nodiscard]] constexpr T& operator*() const noexcept {
       assert(m_ptr);
-      #endif
       return *m_ptr;
     }
 
     /// @brief Returns the contained reference.
-    /// @details Unlike operator*, This function performs a check for null and will throw
-    /// a bad_optional_reference_access exception if *this is empty.
-    constexpr T& ref() const {
+    /// @exception bad_optional_reference_access - If *this is empty.
+    [[nodiscard]] constexpr T& ref() const {
       if (!m_ptr) throw bad_optional_reference_access();
       return *m_ptr;
     }
 
-    /// @brief Returns the contained reference as a pointer.
+    /// @brief Returns the contained reference with pointer syxtax.
     /// @details Note that this function is unchecked and can return nullptr if *this is empty.
-    constexpr T* operator->() const noexcept {
-      #ifndef NDEBUG
+    [[nodiscard]] constexpr T* operator->() const noexcept {
       assert(m_ptr);
-      #endif
+      return m_ptr;
+    }
+
+    /// Returns a pointer to the referenced value or nullptr.
+    [[nodiscard]] constexpr T* ptr() const noexcept {
       return m_ptr;
     }
 
@@ -128,20 +125,18 @@ namespace dl {
   private:
     T* m_ptr;
 
-  };
+  }; // template class optional_reference
 
 
-  /// @brief Returns an optional_reference object holding the passed reference.
-  /// @param reference - the reference to be stored inside the object.
+  /// Returns an optional_reference object holding the passed reference.
   template <class T>
-  constexpr optional_reference<T> opt_ref(T& reference) noexcept {
+  [[nodiscard]] constexpr optional_reference<T> opt_ref(T& reference) noexcept {
     return optional_reference<T>(reference);
   }
 
-  /// @brief Returns an optional_reference object holding an immutable version of the passed reference.
-  /// @param reference - the reference to be stored inside the object.
+  /// Returns an optional_reference object holding an immutable version of the passed reference.
   template <class T>
-  constexpr optional_reference<const T> opt_cref(const T& reference) noexcept {
+  [[nodiscard]] constexpr optional_reference<const T> opt_cref(const T& reference) noexcept {
     return optional_reference<const T>(reference);
   }
 
